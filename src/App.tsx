b6dccf5673bac8f311656749b0be609ee5b14b66
RaiderII <ramiroherrera.utn@gmail.com>
2020-10-04 17:00:01 -0300

    basic addition

--- src/App.tsx
+++ src/App.tsx
@@ -3,30 +3,76 @@ import React, { useState } from 'react';
 function App() {
   interface State {
     input: null | string;
+    prevNum: null | string | number;
     operator: null | string;
   }
-  const initialState = { input: null, operator: null };
+
+  function calculate(operator: string | null, input: any, prevNum: any) {
+    if (operator === '-') {
+      return parseInt(prevNum) - parseInt(input);
+    }
+    if (operator === '+') {
+      return parseInt(prevNum) + parseInt(input);
+    }
+  }
+
+  const initialState = { input: null, prevNum: null, operator: null };
   const [operation, setOperation] = useState<State>(initialState);
 
   function addNum(num: string) {
-    if (num === '0') return;
+    if (num === '0' && !operation.input) return;
 
     if (!operation.input && !operation.operator) {
       setOperation((prevState) => ({
         input: num,
+        prevNum: null,
         operator: null,
       }));
+      return;
+    }
+
+    if (!operation.input && operation.operator) {
+      setOperation((prevState) => ({
+        input: num,
+        prevNum: prevState.prevNum,
+        operator: prevState.operator,
+      }));
+      return;
     }
 
     if (operation.input) {
       setOperation((prevState) => ({
         input: prevState.input + num,
+        prevNum: null,
         operator: prevState.operator,
       }));
+      return;
     }
   }
 
-  function addOperation(userInput: string) {}
+  function addOperation(userInput: string) {
+    if (!operation.prevNum) {
+      setOperation((prevState) => ({
+        input: null,
+        prevNum: prevState.input,
+        operator: userInput,
+      }));
+      return;
+    }
+
+    if (operation.prevNum !== null) {
+      setOperation(({ operator, input, prevNum }) => ({
+        input: null,
+        prevNum: calculate(operator, input, prevNum!.toString())!.toString(),
+        operator: userInput,
+      }));
+      return;
+    }
+  }
+
+  function reset() {
+    setOperation(initialState);
+  }
 
   return (
     <div>
@@ -36,6 +82,7 @@ function App() {
       <button onClick={() => addNum('2')}>2</button>
       <button onClick={() => addOperation('+')}>+</button>
       <button onClick={() => addOperation('-')}>-</button>
+      <button onClick={reset}>AC</button>
     </div>
   );
 }
