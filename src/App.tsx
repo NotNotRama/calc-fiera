5a6c2d91ed1347eba2a63fc50eddb4280ce27568
RaiderII <ramiroherrera.utn@gmail.com>
2020-10-04 15:50:03 -0300

    added buttons and basic functions

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
 import React from 'react';
 
 function App() {
-  return <div className="App">hello</div>;
+  function addNum(num: string) {
+    console.log('some test');
+  }
+
+  function addOperation(userInput: string) {}
+
+  return (
+    <div>
+      <div></div>
+      <button onClick={() => addNum('0')}>0</button>
+      <button onClick={() => addNum('1')}>1</button>
+      <button onClick={() => addNum('2')}>2</button>
+      <button onClick={() => addOperation('+')}>+</button>
+      <button onClick={() => addOperation('-')}>-</button>
+    </div>
+  );
 }
 
 export default App;
